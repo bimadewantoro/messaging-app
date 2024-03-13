ca4dc82692bfc34d6c8828f3c4ffd4757fc27dac
Bima Dewantoro <bimadewantoro22@gmail.com>
2024-03-13 22:13:55 +0700

    feat: [BD] AuthForm UI

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,11 +1,15 @@
 'use client'
 
+import Button from "@/app/components/Button";
+import Input from "@/app/components/inputs/Input";
 import { useCallback, useState } from "react";
 import { 
     FieldValues, 
     SubmitHandler, 
     useForm 
 } from "react-hook-form";
+import AuthSocialButton from "./AuthSocialButton";
+import { BsGithub, BsGoogle } from "react-icons/bs";
 
 type Variant = "LOGIN" | "REGISTER"
 
@@ -76,8 +80,87 @@ const AuthForm = () => {
                     className="space-y-6"
                     onSubmit={handleSubmit(onSubmit)}
                 >
-
+                    {variant === 'REGISTER' && (
+                        <Input 
+                            id="name"
+                            label="Name" 
+                            register={register}
+                            errors={errors}
+                            disabled={isLoading}
+                        />
+                    )}
+                    <Input 
+                        id="email"
+                        label="Email Address"
+                        type="email" 
+                        register={register}
+                        errors={errors}
+                        disabled={isLoading}
+                    />
+                    <Input 
+                        id="password"
+                        label="Password"
+                        type="password" 
+                        register={register}
+                        errors={errors}
+                        disabled={isLoading}
+                    />
+                    <div>
+                        <Button
+                            disabled={isLoading}
+                            fullWidth
+                            type="submit"
+                        >
+                            {variant === 'LOGIN' ? 'Sign In' : 'Register'}
+                        </Button>
+                    </div>
                 </form>
+                <div className="mt-6">
+                    <div className="relative">
+                        <div className="
+                            absolute
+                            inset-0
+                            flex
+                            items-center
+                        ">
+                            <div className="w-full border-t border-gray-300"/>
+                        </div>
+                        <div className="relative flex justify-center text-sm">
+                            <span className="bg-white px-2 text-gray-500">
+                                Or countinue with 
+                            </span>
+                        </div>
+                    </div>
+                    <div className="mt-6 flex gap-2">
+                        <AuthSocialButton 
+                            icon={BsGithub}
+                            onClick={() => socialAction('github')}
+                        />
+                        <AuthSocialButton 
+                            icon={BsGoogle}
+                            onClick={() => socialAction('google')}
+                        />
+                    </div>
+                </div>
+                <div className="
+                    flex
+                    gap-2
+                    justify-center
+                    text-sm
+                    mt-6
+                    px-2
+                    text-gray-500
+                ">
+                    <div>
+                        {variant === 'LOGIN' ? 'New to MessagingApp?' : 'Already have an account?'}
+                    </div>
+                    <div
+                        onClick={toggleVariant}
+                        className="underline cursor-pointer"
+                    >
+                        {variant === 'LOGIN' ? 'Create an account' : 'Sign in'}
+                    </div>
+                </div>
             </div>
         </div>
     );

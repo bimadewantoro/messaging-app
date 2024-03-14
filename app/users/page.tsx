78ccabcfe434d20ce54af19f887407a75571da25
Bima Dewantoro <bimadewantoro22@gmail.com>
2024-03-14 14:05:59 +0700

    feat: [BD] logout and middleware logout

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,12 @@
+'use client';
+
+import { signOut } from "next-auth/react";
+
 const Users = () => {
     return (
-        <div>
-            Hello User !
-        </div>
+        <button onClick={() => signOut()}>
+            Logout
+        </button>
     );
 }
 

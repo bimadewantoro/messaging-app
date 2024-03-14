556c1b0f65f2a4281c8ec55320b5435d39709405
Bima Dewantoro <bimadewantoro22@gmail.com>
2024-03-14 07:27:39 +0700

    feat: [BD] change app name

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Messaging App",
-  description: "New Real Time Messaging App.",
+  title: "Verbalize",
+  description: "Secure your chats, your way. Step into Verbalize for better privacy.",
 };
 
 export default function RootLayout({
